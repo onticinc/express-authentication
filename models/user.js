b0f039d6d137f3f0843afe884e36e0eb0d4f592c
onticinc <onticinc@gmail.com>
2021-12-06 17:56:15 -0700

    push before setting up template

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 'use strict';
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const {
   Model
@@ -37,7 +37,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       validate: {
         len: {
-          args: [8,99],
+          args: [8, 99],
           msg: 'Password must be between 8 and 99 characters'
         }
       }
@@ -54,18 +54,18 @@ module.exports = (sequelize, DataTypes) => {
     pendingUser.password = hash; // this will go to the DB
   });
 
-   // Check the password on Sign-In and compare it to the hashed password in the DB
-  User.prototype.validPassword = function(typedPassword) {
+  // Check the password on Sign-In and compare it to the hashed password in the DB
+  User.prototype.validPassword = function (typedPassword) {
     let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
 
     return isCorrectPassword;
   }
 
   // return an object from the database of the user without the encrypted password
-  User.prototype.toJSON = function() {
-    let userData = this.get(); 
+  User.prototype.toJSON = function () {
+    let userData = this.get();
     delete userData.password; // it doesn't delete password from database, only removes it. 
-    
+
     return userData;
   }
   return User; // add functions above
